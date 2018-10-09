e67ea79694df16efde99a057672b9dfd38b63d60
skyisbule <heqiut@qq.com>
2018-10-09 10:03:32 +0800

    接入七牛

---
+++ src/main/java/com/github/skyisbule/wxpay/qiniu/Uploader.java
@@ -0,0 +1,28 @@
+package com.github.skyisbule.wxpay.qiniu;
+
+import com.qiniu.util.Auth;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Uploader {
+
+
+    @Value("${qiniu.accessKey}")
+    private String accessKey;
+    @Value("${qiniu.secretKey}")
+    private String secretKey;
+    @Value("${qiniu.bucket}")
+    private String bucket;
+
+
+    @RequestMapping("/qiniu/upload")
+    public String getToken(String picName){
+        if (picName==null) return "{\"code\":400,\"dsc\":\"图片名称为空\"}";
+        Auth auth = Auth.create(accessKey, secretKey);
+        String upToken = auth.uploadToken(bucket, picName);
+        return upToken;
+    }
+
+}
